13a325cc67cba758a59e5340e2c3315713d316a7
jlnikhilrao <nikhilraojl@outlook.com>
2024-01-27 13:51:20 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// TODO: run on load, use browser register event
+// TODO: find better way to redirect 
+// TODO: some urls may need to avoid replace to keep page history
+// TODO: Try something like maps within maps
+const url_map_diff_domain = {
+	"twitter.com": "nitter.net",
+	"x.com": "nitter.net",
+	"reddit.com": "old.reddit.com",
+	"www.reddit.com": "old.reddit.com"
+}
+const url_map_same_domain = ["www.youtube.com", "youtube.com"];
+
+let hostname = window.location.hostname;
+if (Object.keys(url_map_diff_domain).includes(hostname)) {
+	// Redirect to different domains
+	let redirect_to = new URL(window.location);
+	redirect_to.hostname = url_map_diff_domain[hostname];
+	window.location.replace(redirect_to);
+}
+
+if (url_map_same_domain.includes(hostname)) {
+	// handle same domain urls
+	let redirect_to = new URL(window.location);
+	if (redirect_to.pathname === '/') {
+		console.log("Getting here");
+		redirect_to.pathname = '/feed/subscriptions/'
+		window.location.replace(redirect_to);
+	}
+}
